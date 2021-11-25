e898bc5ee6551d7d3eb0a882c57ca3e1c59809bb
hamzahamzii <hmza.ehmad@gmail.com>
2021-11-25 16:19:18 +0500

    Transitions added

--- test-nuxt-app/components/JobOptions.vue
+++ test-nuxt-app/components/JobOptions.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="flex flex-col py-4 px-8">
-    <transition>
-      <div
-        class="
-          text-xl
-          transition-all
-          ease-in-out
-          duration-1000
-          text-gray-600 text-center
-        "
-        v-html="selectedTab.title"
-      ></div>
+    <div
+      class="
+        text-xl
+        transition-all
+        ease-in-out
+        duration-1000
+        text-gray-600 text-center
+      "
+      v-html="selectedTab.title"
+    ></div>
+    <transition name="fade">
+      <div v-if="show">
+        <cardOne :cardData="selectedTab.cards[0]" />
+        <cardTwo :cardData="selectedTab.cards[1]" />
+        <cardThree :cardData="selectedTab.cards[2]" />
+      </div>
     </transition>
-    <cardOne :cardData="selectedTab.cards[0]" />
-    <cardTwo :cardData="selectedTab.cards[1]" />
-    <cardThree :cardData="selectedTab.cards[2]" />
   </div>
 </template>
 
@@ -25,22 +27,31 @@ import cardThree from './cards/cardThree.vue'
 
 export default {
   props: ['selectedTab'],
+  data() {
+    return {
+      show: true,
+    }
+  },
   components: {
     cardOne,
     cardTwo,
     cardThree,
   },
+  watch: {
+    selectedTab() {
+      this.show = false
+      setTimeout(() => (this.show = true), 50)
+    },
+  },
 }
 </script>
 
 <style scoped>
-.enter-from {
-  opacity: 0;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-in-out;
 }
-.enter-to {
-  opacity: 1;
-}
-.enter-active {
-  transition: opacity 2s ease;
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0.5;
 }
 </style>
